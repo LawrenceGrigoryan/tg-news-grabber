66cd8a40168ed05c01df064d60035a6bb73463e0
LawrenceGrigoryan <glavrentiy123@gmail.com>
2022-12-27 14:23:03 +0000

    changed grabber code

--- src/run_tg_grabber.py
+++ src/run_tg_grabber.py
@@ -1,12 +1,12 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import NoReturn
 import json
-# import argparse
+import argparse
 
 import pandas as pd
 from omegaconf import OmegaConf
-# Telethon API
+from telethon.errors import SessionPasswordNeededError
 from telethon.sync import TelegramClient
-# Class to work with channel
 from telethon.tl.functions.messages import GetHistoryRequest
 
 from utils import getLogger
@@ -27,77 +27,95 @@ username = config.telegram.username
 phone = config.telegram.phone
 bot_token = config.telegram.bot_token
 # Grabber parameters
-offset_msg = config.grabber.offset_msg
 limit_msg = config.grabber.limit_msg
-total_count_limit = config.grabber.total_count_limit
 
 logger.info('Create a telegram client')
 # TG API client
 client = TelegramClient('../logs/' + username, api_id, api_hash)
 client.start(phone=phone)
-# client.log_out()
+# client = TelegramClient('../logs/' + username, api_id, api_hash)
+# client.start(bot_token=bot_token)
 
 
 class DateTimeEncoder(json.JSONEncoder):
     """
     To serialize dates to JSON
     """
-    def default(self, o):
+    def default(self, o) -> json.JSONEncoder.default:
         if isinstance(o, datetime):
             return o.isoformat()
         if isinstance(o, bytes):
             return list(o)
         return json.JSONEncoder.default(self, o)
 
-        
-async def dump_all_messages(channel, channel_name, out_file_name, offset_msg=0, limit_msg=100, total_count_limit=100):
+
+async def dump_all_messages(
+        channel: str, 
+        channel_name: str,
+        out_file_name: str,
+        limit_msg: int
+        ) -> NoReturn:
     """
     Write messages from channel to a json file
     """
+    # target_date = (datetime.today() + timedelta(hours=3)).date()
+    target_date = datetime.today().date()
+    date_before = target_date - timedelta(days=2)
+    date_before = datetime(
+        date_before.year, 
+        date_before.month, 
+        date_before.day
+    )
     all_messages = []
-    total_messages = 0
     while True:
         history = await client(GetHistoryRequest(
             peer=channel,
-            offset_id=offset_msg,
-            offset_date=None,
+            offset_id=0,
+            offset_date=target_date,
             add_offset=0,
             limit=limit_msg,
             max_id=0,
             min_id=0,
-            hash=0
+            hash=0,
         ))
-        # print(history.stringify())
         if not history.messages:
-            break 
+            break
+
         messages = history.messages
         for message in messages:
             message = message.to_dict()
             message_prepared = {}
-            # Try to get the text of a post
+            # Get messages until unrequired date met
             try:
-                message_prepared['id'] = message['id'] 
-                message_prepared['channel_id'] = message['peer_id']['channel_id']
-                message_prepared['channel_name'] = channel_name
-                message_prepared['date'] = message['date']
-                message_prepared['text'] = message['message']
-                all_messages.append(message_prepared)
+                date_msk = message['date'].replace(tzinfo=None) + timedelta(hours=3)
+                if date_msk > date_before:
+                    message_prepared['id'] = message['id'] 
+                    message_prepared['channel_id'] = message['peer_id']['channel_id']
+                    message_prepared['channel_name'] = channel_name
+                    message_prepared['date'] = date_msk
+                    message_prepared['text'] = message['message']
+                    all_messages.append(message_prepared)
+                else:
+                    total_messages = len(all_messages)
+                    logger.info(
+                        "Number of grabbed news from channel '{}': {}". \
+                            format(channel_name, total_messages)
+                    )
+                    logger.info(
+                        "Saving data from channel '{}' to database". \
+                            format(channel_name)
+                    )
+                    with open('../output/' + out_file_name, 'w', encoding='utf-8') as out_file:
+                        # Create dataframe from news records
+                        news_df = pd.DataFrame.from_records(all_messages)
+                        # Save dataframe to MySQL table
+                        save_table_mysql(df=news_df, conf=database_config)
+                        # Save as json locally
+                        json.dump(all_messages, out_file, ensure_ascii=False, cls=DateTimeEncoder)
+                    return
             # There can be a post with no text (only pictures)
             except KeyError:
                 continue
-        offset_msg = messages[len(messages) - 1].id
-        total_messages = len(all_messages)
-        if total_count_limit != 0 and total_messages >= total_count_limit:
-            break
-
-    logger.info('Saving data to database')
-    with open('../output/' + out_file_name, 'w', encoding='utf-8') as out_file:
-        # Create dataframe from news records
-        news_df = pd.DataFrame.from_records(all_messages)
-        # Save dataframe to MySQL table
-        save_table_mysql(df=news_df, conf=database_config)
-        # Save as json locally
-        json.dump(all_messages, out_file, ensure_ascii=False, cls=DateTimeEncoder)
 
 
 async def main():
@@ -108,10 +126,14 @@ async def main():
     for url in urls:
         channel_name = url.replace('https://t.me/', '')
         channel = await client.get_entity(url)
-        await dump_all_messages(channel, channel_name=channel_name, out_file_name=channel_name + '.json', 
-                                offset_msg=offset_msg, limit_msg=limit_msg, total_count_limit=total_count_limit)
+        await dump_all_messages(
+            channel, 
+            channel_name=channel_name, 
+            out_file_name=channel_name + '.json', 
+            limit_msg=limit_msg
+        )
 
 
-logger.info('Run channel message grabber function')
+logger.info('Running channel message grabber')
 with client:
     client.loop.run_until_complete(main())
